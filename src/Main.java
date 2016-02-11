c89ebd03255c4669359df3bb44c09d52204cdf1f
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2016-02-11 19:30:39 +0100

    Read input

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,23 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Alexandre on 11/02/16.
- */
 public class Main {
-    private static final String FILE_IN = "src/test.in";
+    private static final String FILE_IN = "src/busy_day.in";
+    private static final String FILE_IN2 = "src/redundancy.in";
+    private static final String FILE_IN3 = "src/mother_of_all_warehouses.in";
     private static final String FILE_OUT = "src/test.out";
+    private static int rows;
+    private static int cols;
+    private static int dronesCount;
+    private static int turns;
+    private static int maxLoad;
+    private static int productTypes;
+    private static int[] productWeigths;
+    private static int warehouseCount;
+    private static ArrayList<WareHouse> warehouses;
+    private static int orderCount;
+    private static ArrayList<Order> orders;
+    private static ArrayList<Drone> drones = new ArrayList<>();
 
     public static void main(String[] args) {
         readFile();
@@ -23,6 +34,63 @@ public class Main {
             BufferedReader br = new BufferedReader(new FileReader(FILE_IN));
 
             String line;
+
+            line = br.readLine();
+            String[] ints = line.split(" ");
+            rows = Integer.parseInt(ints[0]);
+            cols = Integer.parseInt(ints[1]);
+            dronesCount = Integer.parseInt(ints[2]);
+            turns = Integer.parseInt(ints[3]);
+            maxLoad = Integer.parseInt(ints[4]);
+
+            productTypes = Integer.parseInt(br.readLine());
+
+            productWeigths = new int[productTypes];
+            ints = br.readLine().split(" ");
+            for (int i = 0; i < productTypes; i++) {
+                productWeigths[i] = Integer.parseInt(ints[i]);
+            }
+
+            warehouseCount = Integer.parseInt(br.readLine());
+            warehouses = new ArrayList<>();
+            for (int i = 0; i < warehouseCount; i++) {
+                ints = br.readLine().split(" ");
+                WareHouse w = new WareHouse();
+                w.x = Integer.parseInt(ints[0]);
+                w.y = Integer.parseInt(ints[1]);
+
+                ints = br.readLine().split(" ");
+                for (int j = 0; j < productTypes; j++) {
+                    int n = Integer.parseInt(ints[j]);
+                    for (int k = 0; k < n; k++) w.products.add(new Product(j, productWeigths[j]));
+                }
+
+                warehouses.add(w);
+            }
+
+            orderCount = Integer.parseInt(br.readLine());
+
+            orders = new ArrayList<>();
+            for (int i = 0; i < orderCount; i++) {
+                Order order = new Order();
+                ints = br.readLine().split(" ");
+                order.x = Integer.parseInt(ints[0]);
+                order.y = Integer.parseInt(ints[1]);
+
+                int n = Integer.parseInt(br.readLine());
+                ints = br.readLine().split(" ");
+                for (int j = 0; j < n; j++) {
+                    int type = Integer.parseInt(ints[j]);
+                    order.products.add(new Product(type, productWeigths[type]));
+                }
+
+                orders.add(order);
+            }
+
+            for (int i = 0; i < dronesCount; i++) {
+                drones.add(new Drone(warehouses.get(0).x, warehouses.get(0).y));
+            }
+
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
@@ -34,7 +102,7 @@ public class Main {
     private static void writeFile() {
         try {
             PrintWriter writer = new PrintWriter(FILE_OUT);
-            writer.println("stuff");
+            writer.println("lol");
             writer.close();
         } catch (IOException e) {
             System.out.println("error writing file " + e.getMessage());
@@ -45,6 +113,11 @@ public class Main {
     private static class Product {
         int weight;
         int type;
+
+        public Product(int type, int weigth) {
+            this.weight = weigth;
+            this.type = type;
+        }
     }
 
     private static class Order {
@@ -53,12 +126,17 @@ public class Main {
     }
 
     private static class WareHouse {
-        List<Product> products;
+        List<Product> products = new ArrayList<>();
         int x, y;
     }
 
     private static class Drone {
-        List<Product> products;
+        List<Product> products = new ArrayList<>();
         int x, y;
+
+        public Drone(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
